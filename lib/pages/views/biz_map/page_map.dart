3907b1065a818ef999fefa5e8479e666f7591bf8
Kening Sun <keningsun@outlook.com>
2021-09-23 16:13:14 -0500

    feat(maps): add tech map

--- lib/pages/views/biz_map/page_map.dart
+++ lib/pages/views/biz_map/page_map.dart
@@ -31,131 +31,164 @@ class _PageMapState extends State<PageMap> {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
-            child: Text(
-              "Interactive Maps",
-              style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-          ),
-          InkWell(
-            onTap: () => context.pushRoute(PageWebBrowse(url: 'https://maps.northwestern.edu/evanston')),
-            child: Container(
-              margin:
-                  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
-              height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    decoration: BoxDecoration(
-                      borderRadius: TDKRadii.r10,
-                      color: Colors.white,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                          'assets/images/our-places.jpg',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    decoration: BoxDecoration(
-                      borderRadius: TDKRadii.r10,
-                      color: Color(0xbc4e2a84),
-                    ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Evanston Campus >',
-                          style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => context.pushRoute(PageWebBrowse(url: 'https://maps.northwestern.edu/chicago')),
-            child: Container(
-              margin:
-                  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
-              height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    decoration: BoxDecoration(
-                      borderRadius: TDKRadii.r10,
-                      color: Colors.white,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                          'assets/images/chicago-sm.jpg',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    decoration: BoxDecoration(
-                      borderRadius: TDKRadii.r10,
-                      color: Color(0xbc4e2a84),
-                    ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Chicago Campus >',
-                          style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
-            child: Text(
-              "Printable Maps (PDF View)",
-              style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-          ),
-          InkWell(
-            onTap: () => context.pushRoute(PageMapPdf(title: 'Evanston Campus', path: 'assets/PDF/EV_campusmap.pdf')),
-            child: Container(
+      body: Container(
+        color: Colors.white,
+        child: ListView(
+          children: [
+            Container(
               margin:
                   EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
               child: Text(
-                "Evanston Campus Map",
-                style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.bold,
-                    color: NUColors.NUPurple,
-                    fontStyle: FontStyle.italic),
+                "Campus Interactive Maps",
+                style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
               ),
             ),
-          ),
-        ],
+            InkWell(
+              onTap: () => context.pushRoute(PageWebBrowse(url: 'https://maps.northwestern.edu/evanston')),
+              child: Container(
+                margin:
+                    EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+                height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      decoration: BoxDecoration(
+                        borderRadius: TDKRadii.r10,
+                        color: Colors.white,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            'assets/images/our-places.jpg',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      decoration: BoxDecoration(
+                        borderRadius: TDKRadii.r10,
+                        color: Color(0xbc4e2a84),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Evanston Campus >',
+                            style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () => context.pushRoute(PageWebBrowse(url: 'https://maps.northwestern.edu/chicago')),
+              child: Container(
+                margin:
+                    EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+                height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      decoration: BoxDecoration(
+                        borderRadius: TDKRadii.r10,
+                        color: Colors.white,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            'assets/images/chicago-sm.jpg',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      decoration: BoxDecoration(
+                        borderRadius: TDKRadii.r10,
+                        color: Color(0xbc4e2a84),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.width * (1 - 2 * MARGIN_RATIO) * 0.4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Chicago Campus >',
+                            style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin:
+                  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+              child: Text(
+                "Printable Maps (PDF View)",
+                style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
+            InkWell(
+              onTap: () => context.pushRoute(PageMapPdf(title: 'Evanston Campus', path: 'assets/PDF/EV_campusmap.pdf')),
+              child: Container(
+                margin:
+                    EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+                child: Text(
+                  "Evanston Campus Map",
+                  style: TextStyle(
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.bold,
+                      color: NUColors.NUPurple,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            Container(
+              margin:
+                  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+              child: Text(
+                "Technological Institute (“Tech”) Building",
+                style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
+            floor(context, 'Basement >', 'Tech Basement', 'assets/PDF/tech/floorB.pdf'),
+            floor(context, 'Ground Floor >', 'Tech Ground Floor', 'assets/PDF/tech/floorG.pdf'),
+            floor(context, 'First Floor >', 'Tech First Floor', 'assets/PDF/tech/floor1.pdf'),
+            floor(context, 'Second Floor >', 'Tech Second Floor', 'assets/PDF/tech/floor2.pdf'),
+            floor(context, 'Third Floor >', 'Tech Third Floor', 'assets/PDF/tech/floor3.pdf'),
+            floor(context, 'Fourth Floor >', 'Tech Fourth Floor', 'assets/PDF/tech/floor4.pdf'),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget floor(BuildContext context, String title, String mapTitle, String path) {
+  return InkWell(
+    onTap: () => context.pushRoute(PageMapPdf(title: mapTitle, path: path)),
+    child: Container(
+      margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO, vertical: 15.sp),
+      child: Text(
+        title,
+        style: TextStyle(
+            fontSize: 30.sp, fontWeight: FontWeight.bold, color: NUColors.NUPurple, fontStyle: FontStyle.italic),
+      ),
+    ),
+  );
 }
