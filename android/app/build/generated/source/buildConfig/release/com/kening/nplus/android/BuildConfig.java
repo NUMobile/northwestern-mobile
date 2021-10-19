c1c13ea7cf92bc6cb8c6f11c6301d7b32ce68ac4
Kening Sun <keningsun@outlook.com>
2021-10-19 16:57:15 -0500

    android release

--- android/app/build/generated/source/buildConfig/release/com/kening/nplus/android/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/kening/nplus/android/BuildConfig.java
@@ -5,7 +5,7 @@ package com.kening.nplus.android;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.thindek.nplus.android";
+  public static final String APPLICATION_ID = "com.kening.nplus.android";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0.0";
