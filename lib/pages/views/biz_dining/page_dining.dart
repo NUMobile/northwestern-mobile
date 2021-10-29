d5ff4c1c9c87d3f2a3c57b9fbc27f9a6d2cab737
Kening Sun <keningsun@outlook.com>
2021-10-29 06:00:57 -0500

    feat(dining): add dining function

---
+++ lib/pages/views/biz_dining/page_dining.dart
@@ -0,0 +1,228 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nu_mobile/router/router.gr.dart';
+import 'package:nu_mobile/utils/colors.dart';
+import 'package:thindek_ui/thindek_ui.dart';
+
+const String jscodeFaq =
+    "document.getElementsByTagName('header')[0].style.display = 'none';";
+
+class PageDining extends StatefulWidget {
+  const PageDining({Key? key}) : super(key: key);
+
+  @override
+  _PageDiningState createState() => _PageDiningState();
+}
+
+class _PageDiningState extends State<PageDining> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: NUColors.Purple80,
+        ),
+        title: Text(
+          'Dining',
+          style: TextStyle(
+            color: NUColors.Purple80,
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: ListView(
+          children: [
+            // libraryTitle(context, 'NU Shuttle Tracker'),
+            ListTile(
+              leading: Icon(
+                Icons.local_dining,
+                color: NUColors.Purple90,
+                size: 30,
+              ),
+              onTap: () {
+                context.pushRoute(PageWebView(
+                  title: 'Menu',
+                  url:
+                      'https://dineoncampus.com/northwestern/whats-on-the-menu',
+                ));
+              },
+              title: Text(
+                'What\'s On The Menu?',
+                style: TextStyle(
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 30.sp,
+              ),
+            ),
+            tdkDivider(context),
+            ListTile(
+              leading: Icon(
+                Icons.access_time,
+                color: NUColors.Purple90,
+                size: 30,
+              ),
+              onTap: () {
+                context.pushRoute(PageWebView(
+                  title: 'Hours of Operation',
+                  url:
+                      'https://dineoncampus.com/northwestern/hours-of-operation',
+                ));
+              },
+              title: Text(
+                'Hours of Operation',
+                style: TextStyle(
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 30.sp,
+              ),
+            ),
+            tdkDivider(context),
+            ListTile(
+              leading: Icon(
+                Icons.help_outline_rounded,
+                color: NUColors.Purple90,
+                size: 30,
+              ),
+              onTap: () {
+                context.pushRoute(PageWebView(
+                    title: 'Meal Plan FAQs',
+                    url: 'https://dineoncampus.com/northwestern/meal-plan-faqs',
+                    jscode: jscodeFaq));
+              },
+              title: Text(
+                'Meal Plan FAQs',
+                style: TextStyle(
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 30.sp,
+              ),
+            ),
+            tdkDivider(context),
+            ListTile(
+              leading: Icon(
+                Icons.attach_money_rounded,
+                color: NUColors.Purple90,
+                size: 30,
+              ),
+              onTap: () {
+                context.pushRoute(PageWebBrowse(
+                  url: 'https://get.cbord.com/northwestern/full/funds_home.php',
+                ));
+              },
+              title: Text(
+                'Balance Check (GET)',
+                style: TextStyle(
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 30.sp,
+              ),
+            ),
+            tdkDivider(context),
+            ListTile(
+              leading: Icon(
+                Icons.apps_rounded,
+                color: NUColors.Purple90,
+                size: 30,
+              ),
+              onTap: () {
+                context.pushRoute(
+                  PageWebBrowse(
+                      url: "https://dineoncampus.com/northwestern",
+                      jscode: jscodeFaq),
+                );
+              },
+              title: Text(
+                'More Dining Info >',
+                style: TextStyle(
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              height: 20.sp,
+            ),
+            // libraryImage(context, 'assets/images/shuttle1440.jpg'),
+            // InkWell(
+            //   onTap: () => context.pushRoute(PageMapPdf(
+            //       title: 'Evanston Campus',
+            //       path: 'assets/PDF/shuttle-overview.pdf')),
+            //   child: Container(
+            //     margin: EdgeInsets.symmetric(
+            //         horizontal:
+            //             MediaQuery.of(context).size.width * MARGIN_RATIO,
+            //         vertical: 15.sp),
+            //     child: Text(
+            //       "Northwestern Shuttles 2020-2021 >",
+            //       style: TextStyle(
+            //           fontSize: 30.sp,
+            //           fontWeight: FontWeight.bold,
+            //           color: NUColors.NUPurple,
+            //           fontStyle: FontStyle.italic),
+            //     ),
+            //   ),
+            // ),
+            // libraryDescription(
+            //     'Northwestern University operates several shuttles for students, faculty, and staff on the Evanston and Chicago campuses. '),
+            // routeMap(context, 'Intercampus >', () {
+            //   context.pushRoute(PageWebView(
+            //     title: 'Intercampus',
+            //     url:
+            //         'https://www.google.com/maps/d/u/0/viewer?mid=1MkGRPsCy0e7vWkyu_WIWChAepU8',
+            //   ));
+            // }, Colors.red),
+            // routeMap(context, 'Evanston Loop >', () {
+            //   context.pushRoute(PageWebView(
+            //     title: 'Evanston Loop',
+            //     url:
+            //         'https://www.google.com/maps/d/u/0/viewer?mid=1Vb6-WLFdIrBg1u2Nqd1kByCeVbU&ll=42.05271836553796%2C-87.6806563&z=15',
+            //   ));
+            // }, NUColors.NUPurple),
+            // routeMap(context, 'Campus Loop >', () {
+            //   context.pushRoute(PageWebView(
+            //     title: 'Campus Loop',
+            //     url:
+            //         'https://www.google.com/maps/d/u/0/viewer?mid=1eloZBnx13Ix-B9WyMSqqevospJc&ll=42.05373021645418%2C-87.67500742900472&z=14',
+            //   ));
+            // }, Colors.green),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Container libraryTitle(BuildContext context, String title) {
+  return Container(
+    margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * MARGIN_RATIO,
+        vertical: 30.sp),
+    child: Text(
+      title,
+      style: TextStyle(
+          fontSize: 40.sp, fontWeight: FontWeight.bold, color: Colors.black),
+    ),
+  );
+}
